b1d6f09be8d29d8ddd2e1bcb08426875cd89cfd8
mohammad ayoub <95487430+Mohammad-Ayoub@users.noreply.github.com>
2024-07-31 20:35:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -229,11 +229,16 @@ st.sidebar.markdown("Leen Al Sayyid")
 # Sidebar with hyperlinked subheader
 st.sidebar.markdown("### [Professor Samar Hajj](https://www.aub.edu.lb/pages/profile.aspx?memberID=sh137)")
 
-st.sidebar.markdown(
-    """
-    [![Alt Text](https://www.google.com/imgres?q=samar%20hajj%20aub&imgurl=https%3A%2F%2Fspservices.aub.edu.lb%2FPublicWebService.svc%2FFMIS_GetProfilePicture%3FmemberId%3Dsh137&imgrefurl=https%3A%2F%2Fwww.aub.edu.lb%2Fpages%2Fprofile.aspx%3FmemberID%3Dsh137&docid=Uw4HZOVvJz-XOM&tbnid=zTyA-3PTW7FkpM&vet=12ahUKEwjD38TT5tGHAxW2VaQEHUvPKOMQM3oECBwQAA..i&w=359&h=362&hcb=2&ved=2ahUKEwjD38TT5tGHAxW2VaQEHUvPKOMQM3oECBwQAA)]
-    """
-)
+# MSBA Logo
+html_string1 = '''<!DOCTYPE html>
+<html>
+<body>
+ <a href="https://www.aub.edu.lb/osb/MSBA/Pages/default.aspx">
+  <img src="https://spservices.aub.edu.lb/PublicWebService.svc/FMIS_GetProfilePicture?memberId=sh137" width=300" height="80" />
+ </a>
+</body>
+</html>'''
+st.sidebar.markdown(html_string1, unsafe_allow_html=True)
 
 
 tab1, tab2, tab3, tab4, tab5, tab6,tab7 = st.tabs(['Geographical Mapping', 'Average ICER by Region', 'Average ICER by Country', 'Intervention Type', 'Egypt Age Group', 'Sudan Age Group','fig_prevalence_icer_filtered'])
